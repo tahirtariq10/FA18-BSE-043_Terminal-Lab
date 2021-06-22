23798008e905bf94a5f60f98541530d718f66803
Tahir Tariq <shehbaztariq1988@gmail.com>
2021-06-22 14:31:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+var productsRouter = require("./routes/products");
+var usersRouter = require("./routes/users");
+var session = require("express-session");
+var sessionAuth = require("./middlewares/sessionAuth");
 var app = express();
-
+app.use(session({ secret: "keyboard cat", cookie: { maxAge: 60000 } }));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(sessionAuth);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/products", productsRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
